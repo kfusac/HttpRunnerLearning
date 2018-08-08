bcc6a174f8a2ef1fc52bd99f68a87e09dd5a7820
kfusac <kfusac@163.com>
2018-08-08 16:42:21 +0800

    test yaml loader for smoke test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -14,12 +14,28 @@ class TestRunner(TestApiServerBase):
     def teardown_class(cls):
         super().teardown_class()
 
+    def setup_method(self):
+        self.reset_all()
+
     def test_run_single_json_testcase(self):
         path = 'tests/testcase/smoke_test.json'
 
         json_testcase = loader.load_json_file(path)
 
         success, diff_content = runner.run_single_testcase(json_testcase)
+        print(diff_content)
+        assert success
+
+    def test_run_single_yaml_testcase(self):
+        path = 'tests/testcase/smoke_test.yml'
+
+        json_testcase = loader.load_yaml_file(path)
+
+        success, diff_content = runner.run_single_testcase(json_testcase)
 
         print(diff_content)
         assert success
+
+    def reset_all(self):
+        url = f'{self.host}/api/reset-all'
+        return self.api_client.get(url)
